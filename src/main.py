e980642f050cf35f3df46c6f62f03578bd852513
Michel Dequick <michel.dequick@gmail.com>
2018-02-22 14:57:51 +0100

    plz kill me

--- src/main.py
+++ src/main.py
@@ -50,7 +50,7 @@ class image_converter:
 
 def main(args):
   ic_rgb = image_converter(topic_rgb, topic_opencv_rgb, "bgr8")
-  ic_depth = image_converter(topic_depth, topic_opencv_depth, "mono8")
+  ic_depth = image_converter(topic_depth, topic_opencv_depth, "MONO8")
 
   rospy.init_node('image_converter', anonymous=True)
   try:
